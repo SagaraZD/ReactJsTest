bd804b5647deceabd1673b23621dbd007b7b8366
SagaraZD <gd.sagara@gmail.com>
2019-01-09 23:02:54 +0530

    using css moudle for style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.module.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -76,20 +76,20 @@ class App extends Component {
 
     //let classes = ['red', 'bold'].join(' ');
     
-    const classes = [];
+    const Assignedclasses = [];
     if (this.state.persons.length <=2){
-      classes.push('red');
+      Assignedclasses.push('red');
     }
     if (this.state.persons.length <=1) {
-      classes.push('bold');
+      Assignedclasses.push('bold');
     }
 
     
     return (
 
-      <div className="App">
+      <div className={classes.App}>
           <h1> Hi, I'm a React App </h1>
-          <p className={classes.join(' ')}> This is really working!</p>
+          <p className={Assignedclasses.join(' ')}> This is really working!</p>
           <button style={style} onClick={this.togglePersonHandler}>Switch Name</button>
 
           {persons}
